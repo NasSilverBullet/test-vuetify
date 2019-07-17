a516c639930bed6246983c1750c400c114321486
y.t <y.softvalley@gmail.com>
2019-07-17 09:37:04 +0900

    first commit

---
+++ src/components/Layout.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app id="inspire" dark>
+    <v-navigation-drawer v-model="drawer" clipped fixed app>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid>
+        <slot name="content">
+          <v-layout justify-center align-center>
+            <h1>Not found</h1>
+          </v-layout>
+        </slot>
+      </v-container>
+    </v-content>
+    <v-footer app fixed>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Layout",
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
